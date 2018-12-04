03c5a91a112e735c615d6b7ecde66819822fb96f
Daniel Sarateanu <daniel.sarateanu@endava.com>
2018-12-04 18:16:02 +0200

    interpreter pattern

---
+++ src/main/java/com/home/patterns/behavioral/interpreter/AbstractExpression.java
@@ -0,0 +1,13 @@
+package com.home.patterns.behavioral.interpreter;
+
+/**
+ *
+ * AbstractExpresion defines interface for interpretation. Interface must be
+ * used by TerminalEpression and NonTerminalEpression.
+ *
+ */
+public abstract class AbstractExpression {
+
+	abstract void interpret(Context context);
+
+}
--- src/main/java/com/home/patterns/behavioral/interpreter/Main.java
+++ src/main/java/com/home/patterns/behavioral/interpreter/Main.java
@@ -2,4 +2,7 @@ package com.home.patterns.behavioral.interpreter;
 
 public class Main {
 
+	public static void main(String[] args) {
+
+	}
 }
