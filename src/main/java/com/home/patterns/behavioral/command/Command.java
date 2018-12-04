c567837dbb773a95b5b50b89bf66d61ac8c61995
Daniel Sarateanu <daniel.sarateanu@endava.com>
2018-12-04 16:25:57 +0200

    command pattern

---
+++ src/main/java/com/home/patterns/behavioral/command/Command.java
@@ -0,0 +1,11 @@
+package com.home.patterns.behavioral.command;
+
+/**
+ *
+ * Command interface, declares an interface for executing an operation
+ *
+ */
+public interface Command {
+
+	void execute();
+}
---
+++ src/main/java/com/home/patterns/behavioral/command/Invoker.java
@@ -0,0 +1,19 @@
+package com.home.patterns.behavioral.command;
+
+/**
+ *
+ * Invoker class, asks the command to carry out the request
+ *
+ */
+public class Invoker {
+
+	private Command command;
+
+	public Invoker(Command command) {
+		this.command = command;
+	}
+
+	public void execute() {
+		command.execute();
+	}
+}
